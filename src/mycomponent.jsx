f47aae1e9b01cd7c247b76b9b394f019fa5cec38
Sven Anders Robbestad <sven@robbestad.com>
2017-02-11 19:45:33 +0100

    Demonstrated store action with didMount and SetInterval

--- src/mycomponent.jsx
+++ src/mycomponent.jsx
@@ -4,6 +4,12 @@ import { connect } from 'inferno-mobx'
 
 @connect(['titleStore'])
 export default class Main extends Component {
+  componentDidMount(){
+    setInterval(()=>{
+      this.props.titleStore.setTitle(new Date().toString())
+    },1000)
+  }
+
   render({ titleStore }) {
     return <div>
       <p>{ titleStore.title }</p>
