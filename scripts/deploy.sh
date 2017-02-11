0cfa71ba355ed402def2ad7b16c4ad637fb2a566
Sven Anders Robbestad <robbestad@gmail.com>
2017-02-11 12:46:59 +0100

    Added deploy script, mocha and skeleton test

---
+++ scripts/deploy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
+git commit -am"Deploy version $PACKAGE_VERSION"
+#git push origin master
---
+++ test/test/example.js
@@ -0,0 +1,10 @@
+/*
+Example file to show how to set up a skeleton test
+ */
+const path = require('path');
+const assert = require('assert');
+describe('Example mocha test', () => {
+    it('verifies that 1 is indeed 1', function() {
+        assert.equal(1, 1);
+    });
+});
