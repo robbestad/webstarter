71741f2a515cc3bc98f8bd05dd4a91da3657a47d
Sven Anders Robbestad <robbestad@gmail.com>
2017-02-11 13:26:17 +0100

    Added favicon

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const koa = require('koa');
+const convert = require('koa-convert');
 const path = require('path');
 const favicon = require('koa-favicon');
 const koaStaticPlus = require('koa-static-plus');
 
 const app = new koa()
-  .use(koaStaticPlus(path.join(__dirname, 'static'), {}));
+  .use(favicon(path.join(__dirname, 'static', 'favicon.ico')))
+  .use(convert(koaStaticPlus(path.join(__dirname, 'static'), {})));
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const koa = require('koa');
 const http = require('http');
 const app = require('./app');
 
 http.createServer(app.callback()).listen(process.env.PORT || 1997);
+
+//eslint-disable-next-line no-console
+console.log("Started webserver on http://localhost:" + (process.env.PORT || 1997));
