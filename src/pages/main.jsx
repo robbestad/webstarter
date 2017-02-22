1b713095ac47a1d4579177636686fa354ce925b0
Sven Anders Robbestad <sven@robbestad.com>
2017-02-22 22:28:47 +0100

    New gundb config

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -6,7 +6,7 @@ import {connect} from 'inferno-mobx'
 @connect(['counter'])
 export default class Main extends Component {
   render() {
-    const { counter } = this.props
+    const { counter } = this.props;
     return t('div', null,
       t('h2', null, 'Hello dear user :)'),
       t('div', null, `${counter.getCount()}`),
--- src/stores/counter.js
+++ src/stores/counter.js
@@ -2,18 +2,24 @@ const {extendObservable, observable} = require('mobx');
 const Gun = require('gun');
 import config from '../config/index';
 const gun = Gun(config.gundb);
-Gun.chain.count = function (num) {
-  if (typeof num === 'number') {
-    this.path(Gun.text.random()).put(num);
-  }
-  if (typeof num === 'function') {
-    let sum = 0;
-    this.map().val(function (val) {
-      num(sum += val);
-    });
-  }
-  return this;
-};
+// Gun.chain.count = function (num) {
+//   if (typeof num === 'number') {
+//     this.path(Gun.text.random()).put(num);
+//   }
+//   if (typeof num === 'function') {
+//     let sum = 0;
+//     this.map().val(function (val) {
+//       num(sum += val);
+//     });
+//   }
+//   return this;
+// };
+// Gun.chain.live = function(cb, opt){
+//   return this.on(function(val, field){
+//     delete val._;
+//     cb.call(this, val, field);
+//   }, opt);
+// };
 
 /**
  * @class Counter
@@ -22,11 +28,20 @@ Gun.chain.count = function (num) {
 export default class Counter {
   constructor(request, state = {}) {
 
-    let db = gun.get("webstarter/counter");
-    db.count( value => {
-      this.setCount(value)
+    let db = gun.get("webstarter/hits");
+
+    let sum = 0;
+
+    db.on((data) => {
+      sum = ~~data.hits;
+      this.setCount(sum + 1);
     });
-    db.count(+1);
+    gun.get('webstarter/hits').put({hits: sum + 1});
+
+    // db.count( value => {
+    //   this.setCount(value)
+    // });
+    // db.count(+1);
 
     // let sum = 0;
     // db.map().val(val => {
@@ -46,7 +61,7 @@ export default class Counter {
 
     this.request = request;
     extendObservable(this, {
-      hits: null
+      hits: ''
     }, state)
   }
 
