72e0ecff639c06c772f0e90171268bbc08c17156
Sven Anders Robbestad <sven@robbestad.com>
2017-02-23 15:56:52 +0100

    New gundb config (9)

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -4,39 +4,44 @@ import config from '../../config/index';
 import {Link} from 'inferno-router';
 import { connect } from 'inferno-mobx'
 
-// @connect(['counter'])
+@connect(['counter'])
 export default class Layout extends Component {
   render({children}) {
+    const { counter } = this.props;
     return <div className="grid">
+
       <div className="row">
-        <div className="col-4 border-left border-bottom">
+        <div className="col-large-4 col-medium-12 col-small-12 border-left border-bottom">
           <div className="content">
             <div className="font-semi-bold font-size-xxl">
               <div className="inner-first">{config.title}</div>
             </div>
           </div>
         </div>
-        <div className="col-3 border-left border-bottom">
+        <div className="col-large-3 border-left border-bottom hide-medium hide-small">
           <div className="content">
-            <div className="font-semi-bold font-size-l italic">
-              <div className="inner"> 1</div>
+            <div className="font-style-regular font-size-l">
+              <div className="inner">Your are<br/>visitor <span className="font-style-extra-bold italic">#{counter.getCount()}</span></div>
             </div>
           </div>
         </div>
-        <div className="col-3 border-left border-bottom">
+        <div className="col-large-3 border-left border-bottom hide-medium hide-small">
           <div className="content ">
             <div className="font-semi-bold font-size-l italic">
               <div className="inner">2</div>
             </div>
           </div>
         </div>
-        <div className="col-2 border-left border-bottom border-right">
+        <div className="col-large-2 col-medium-2 border-left border-bottom border-right hide-medium hide-small">
           <div className="content ">
             <div className="font-semi-bold font-size-l italic">
               <div className="inner"></div>
             </div>
           </div>
         </div>
+
+
+
 
       </div>
       <div className="row">
--- src/stores/counter.js
+++ src/stores/counter.js
@@ -2,18 +2,20 @@ const {extendObservable, observable} = require('mobx');
 const Gun = require('gun');
 import config from '../config/index';
 
-// Gun.chain.count = function (num) {
-//   if (typeof num === 'number') {
-//     this.path(Gun.text.random()).put(num);
-//   }
-//   if (typeof num === 'function') {
-//     let sum = 0;
-//     this.map().val(function (val) {
-//       num(sum += val);
-//     });
-//   }
-//   return this;
-// };
+
+Gun.chain.count = function (num) {
+  if (typeof num === 'number') {
+    this.path(Gun.text.random()).put(num);
+  }
+  if (typeof num === 'function') {
+    var sum = 0;
+    this.map().val(function (val) {
+      num(sum += val);
+    });
+  }
+  return this;
+};
+
 Gun.chain.live = function (cb, opt) {
   return this.on(function (val, field) {
     delete val._;
@@ -29,27 +31,23 @@ export default class Counter {
   constructor(request, state = {}) {
     const gun = Gun(config.gundb);
     console.log('using ' + config.gundb);
-    let db = gun.get('webstarter').path('hits');
+    // let db = gun.get('webstarter').path('hits');
 
     const updateData = (sum) => {
-      gun.get('webstarter').put({hits: sum}, function (ack) {
+      gun.get('webstarter').put({hits: sum}, (ack) => {
         if (ack.err) {
           console.error(ack.err);
         }
+        this.setCount(sum);
         console.log('Data synced');
       });
     };
-
-    let newSum = 0;
-    let hits = 0;
-    db.live(data => {
-      console.log(data);
-      this.setCount(data);
-      hits = data;
-      newSum = ~~data + 1;
+    const db = gun.get('count');
+    let hits = '';
+    db.on((data) => {
+      this.setCount(Object.keys(data).length);
     });
-    updateData(newSum);
-
+    db.count(+1);
     this.request = request;
     extendObservable(this, {
       hits
